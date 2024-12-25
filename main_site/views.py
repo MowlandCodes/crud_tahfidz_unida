9f144676341b927519df46fbbd2cb78fd39ee746
mowlandcodes <mowlandgaming@gmail.com>
2024-12-25 17:16:53 +0700

    added UNIDA logo, and base template sidebar

--- main_site/views.py
+++ main_site/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import LoginForm, SignupForm, SetoranHafalan
 
 # Create your views here.
 def index(response):
-    return HttpResponse("<h1>Hello World, You're at the main page</h1>")
+    return redirect("dashboard")
 
 def dashboard(response):
     return render(response, "main_site/dashboard.html", {})
