bcf4344f789b54b752e144c12d5c64c4f4dffee9
mowlandcodes <mowlandgaming@gmail.com>
2025-01-01 13:19:32 +0700

    added login & logout mechanism and added rate limiting logic for 15 requests/minute

--- main_site/urls.py
+++ main_site/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("dashboard", views.dashboard, name="dashboard"),
-    path("login", views.login, name="login"),
+    path("hafalan", views.hafalan, name="hafalan"),
+    path("login", views.login_page, name="login_page"),
     path("signup", views.signup, name="signup"),
+    path("logout", views.logout_view, name="logout_view"),
 ]
--- main_site/views.py
+++ main_site/views.py
@@ -1,25 +1,51 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.middleware.csrf import get_token
+from django_ratelimit.decorators import ratelimit
 
 # Create your views here.
 def index(request):
-    return redirect("login")
+    return redirect("/login")
 
 def dashboard(request):
-    return render(request, "main_site/dashboard.html", {})
+    if request.user.is_authenticated:
+        return render(request, "main_site/dashboard.html", {})
+    else:
+        return redirect("/login")
 
-def login(request):
+# Limiting login attempts to prevent Brute Force Attack
+@ratelimit(key='ip', rate='15/m', block=True)
+def login_page(request):
+    csrf_token = get_token(request)
+    if request.user.is_authenticated: # Preventing the user to touch login page after successfully logged in
+        return redirect("/dashboard")
+
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
         remember = request.POST.get("remember", False)
 
-        return HttpResponse(f"Username : {username} <br> Password : {password} <br> Remember : {remember}")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")
+        else:
+            messages.error(request, "Invalid username or password")
 
-    else:
-        return render(request, "main_site/login.html", {})
+    return render(request, "main_site/login.html", {"csrf_token" : csrf_token})
 
 def signup(request):
     return render(request, "main_site/signup.html", {})
+
+
+def hafalan(request):
+    return render(request, "main_site/crud_hafalan.html", {})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/login")
