7aa03ad96c0f4144979e310a3af1ae8f740a7c3c
mowlandcodes <mowlandgaming@gmail.com>
2025-01-02 10:57:13 +0700

    changed staff & admin dashboard title

--- main_site/admin.py
+++ main_site/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as text
 
 from .models import Hafalan
 
 # Register your models here.
 admin.site.register(Hafalan)
+
+# Change the admin site header, title, and index title
+admin.site.site_header = text("Tahfidz CRUD Dashboard")
+admin.site.site_title = text("Tahfidz CRUD")
+admin.site.index_title = text("Tahfidz CRUD Dashboard")
