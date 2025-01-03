f4c4320294bea1c357053c8e88891e667a30646d
mowlandcodes <mowlandgaming@gmail.com>
2025-01-03 18:45:17 +0700

    add: opacity in .hidden style to smoothen the animation, add: added profile to list of hidden when collapsed in sidebar

--- main_site/static/main_site/script/base.js
+++ main_site/static/main_site/script/base.js
@@ -1,12 +1,14 @@
 const sidebar = document.querySelector('.sidebar');
 const logo = document.querySelector('.logo-unida');
 const nav_link = document.querySelectorAll('.nav-link');
+const profile = document.querySelector('.profile');
 
 function toggle_sidebar(button) {
     const toggle_sidebar_icon = button.querySelector('i');
     sidebar.classList.toggle('close');
     toggle_sidebar_icon.classList.toggle('rotate');
     logo.classList.toggle('hidden');
+    profile.classList.toggle('hidden');
     for (i = 0; i < nav_link.length; i++) {
         nav_link[i].classList.toggle('hidden');
     }
