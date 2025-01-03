a336b0464ae7ae813d00c38344e3dbcf0e30e3ff
mowlandcodes <mowlandgaming@gmail.com>
2025-01-03 18:34:27 +0700

    add: DataTables for table styling and visualizing

---
+++ main_site/static/main_site/script/hafalan.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("#hafalan-table").DataTable({
+        responsive: true,
+        paging: false,
+        searching: false,
+        scrollCollapse: true,
+        ordering: true,
+        info: true,
+        scrollY: '50vh',
+        language: {
+            search: "Cari data ",
+            lengthMenu: "Menampilkan _MENU_ data per halaman",
+            info: "Menampilkan _START_ - _END_ dari _TOTAL_ data",
+            infoFiltered: "(Terfilter dari _MAX_ total data)",
+            infoEmpty: "Tidak ada hafalan",
+            zeroRecords: "Tidak ada hafalan yang sesuai",
+        },
+        buttons: ['copy', 'excel', 'pdf']
+    });
+});
