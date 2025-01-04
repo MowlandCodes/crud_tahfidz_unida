0748146569c06b8eb045ebed258ccc1a89b56c10
mowlandcodes <mowlandgaming@gmail.com>
2025-01-04 17:19:17 +0700

    Revert "add: added UUID for each user"
    This reverts commit be520d85c56e292e6ba97b4355ba2fc107d46a80.

--- main_site/apps.py
+++ main_site/apps.py
@@ -5,6 +5,3 @@ class MainSiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main_site'
 
-    def ready(self):
-        import main_site.signals
-
--- main_site/signals.py
+++
@@ -1,6 +0,0 @@
-import uuid
-from django.contrib.auth.models import User
-from django.db import models
-
-if not hasattr(User, 'uuid'):
-    User.add_to_class('uuid', models.UUIDField(default=uuid.uuid4, editable=False, unique=True))
