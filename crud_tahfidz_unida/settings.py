ba5a45b85a17147ee2253e61198d64d55e1f3ab3
mowlandcodes <mowlandgaming@gmail.com>
2025-01-06 20:55:31 +0700

    edit: changed secret_key for applications

--- crud_tahfidz_unida/settings.py
+++ crud_tahfidz_unida/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ew!$18_5uzc0fp%&4fc!u1@r*h)mtmxklwd0^2fc+m^&-do&kc'
+# SECRET_KEY = 'django-insecure-ew!$18_5uzc0fp%&4fc!u1@r*h)mtmxklwd0^2fc+m^&-do&kc'
+SECRET_KEY = 'YTdiMDAwYzMxYmY5ZDcwNWUxNmZkM2QzYjEwNDZhODgyZjA2NDcxZmViNmFlOTY5YjNhNjVmODlhOTdkZTY4YQ=='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
